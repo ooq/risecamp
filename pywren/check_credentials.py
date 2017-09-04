cd2ffe0a752474b3a4c64bc9e57d25035bbd6623
Allan Peng <potette007@gmail.com>
2017-09-04 00:21:11 -0700

    env string generation

--- pywren/check_credentials.py
+++ pywren/check_credentials.py
@@ -5,20 +5,20 @@ from multiprocessing import Process
 import pywren
 
 import numpy as np
+import base64
 
 ROOT_DIR = os.getcwd()
 
 CONFIG_PATH = os.path.join(ROOT_DIR, ".pywren_config")
 AWS_CREDS = os.path.join(ROOT_DIR, ".aws/credentials")
 
-CREDS_TO_TRY = os.path.abspath("pywren_creds")
+CREDS_TO_TRY = os.path.abspath("risecamp_pywren_creds")
 
 ROOT_USER = 0
 NUM_SUBUSERS = 20
 
 def test_function(b):
   return "hello world"
-
 
 def verify_user(root, subuser):
   base = "user_%02d.subuser.user_%02d.deploy" % (root, subuser)
@@ -45,7 +45,7 @@ def verify_user(root, subuser):
     raise Exception("Problem executing lambda with account {} subuser {}".format(root, subuser) )
   print("success")
 
-if __name__ == '__main__': 
+def verify_all():
   aws_dir = os.path.join(ROOT_DIR, ".aws")
   if os.path.exists(aws_dir):
     a = input("remove existing .aws creds?[yn]: ")
@@ -68,3 +68,32 @@ if __name__ == '__main__':
   os.unlink(CONFIG_PATH)
   print()
   print("All done")
+
+def base64_encode(config_string):
+  return "PYWREN_CONFIG=" + base64.b64encode(config_string)
+
+def create_env_file(root, subuser, src, target):
+  base = "user_%02d.subuser.user_%02d.deploy" % (root, subuser)
+  
+  pywren_config = base + ".pywren_config.yaml"
+  cred_file = base + ".creds"
+  with open(os.path.join(target, base), "wb") as f:
+    with open(os.path.join(src, cred_file), 'rb') as g:
+      f.write(g.read())
+    f.write("\n")
+    with open(os.path.join(src, pywren_config), 'rb') as h:
+      f.write(base64_encode(h.read()))
+
+def generate_creds(src, target_dir):
+  if os.path.exists(target_dir):
+    a = input("remove existing directory? [yn]: ")
+    if a[0] != 'y':
+      exit()
+    shutil.rmtree(target_dir)
+  os.makedirs(target_dir)
+  for user in range(NUM_SUBUSERS):
+    create_env_file(ROOT_USER, user, src, target_dir)
+
+if __name__ == '__main__':
+  generate_creds(CREDS_TO_TRY, "creds_for_qifan")
+
